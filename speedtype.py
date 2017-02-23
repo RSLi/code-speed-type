93390a67733b26300e5d04d27d47459adf41f837
RSLi <robertsli@163.com>
2017-02-22 23:16:51 -0500

    Initialize Sphinx extension python file basic structure

--- speedtype.py
+++ speedtype.py
@@ -1,3 +1,26 @@
 from docutils import nodes
 from docutils.parsers.rst import directives
 from docutils.parsers.rst import Directive
+
+def setup(app):
+    return {'version': '0.1'}
+
+class SpeedTypeNode(nodes.General, nodes.Element):
+    pass
+
+def visit_st_node(self, node):
+    #todo
+    pass
+
+def depart_st_node(self, node):
+    #todo
+    pass
+
+def process_st_node(app, env, docname):
+    pass
+
+def purge_st(app, env, docname):
+    pass
+
+class SpeedTypeDirective(Directive):
+    pass
