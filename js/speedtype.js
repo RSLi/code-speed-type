3a1c2646828d47b4e3aed7b58fe77b7760281d3e
RSLi <robertsli@163.com>
2017-02-23 17:52:14 -0500

    Complete speedtype basic functionality

--- js/speedtype.js
+++ js/speedtype.js
@@ -2,9 +2,11 @@
  * Author: Siwei "Robert" Li (RSLi)
  * Date: February 23rd, 2017
  * Updated: February 23rd, 2017
- *
- * TODO: be able to bind to other code pieces, to check whether code typed is consistant, and to record time used when start typing
  */
+
+//TODO: Support language other than java
+//TODO: be able to bind to other code pieces, to check whether code typed is consistant
+//TODO: record time used when start typing
 
 function SpeedType(options) {
     if (options) {
@@ -14,7 +16,19 @@ function SpeedType(options) {
 
 SpeedType.prototype.init = function(options) {
     this.options = options;
+    if (options.language) {
+        this.language = options.language;
+    } else {
+        this.language = "clike";
+    }
     this.createEditor();
+
+    //if passed in origElem to be appended, replace it
+    if (this.options.origElem) {
+        this.appendElement(this.options.origElem);
+    }
+
+    this.editor.refresh();
 };
 
 //for debug purpose
@@ -22,19 +36,29 @@ SpeedType.prototype.toString = function() {
     return '[SpeedType] - with options: ' + options;
 };
 
-//Create CodeMirror instance
+//Create CodeMirror instance within wrapper elements
 SpeedType.prototype.createEditor = function() {
     this.containerDiv = document.createElement('div');
     this.editorDiv = document.createElement('div');
 
     //Order DOM elements
-    this.options.origElem.appendChild(containerDiv);
     this.containerDiv.appendChild(this.editorDiv);
 
     //Call CodeMirror API to create editor object
-    this.editor = CodeMirror(editorDiv, {
+    this.editor = CodeMirror(this.editorDiv, {
+        value: "public class",
         lineNumbers: true,
+        mode: this.language,
+        theme: "base16-light",
+        indentUnit: 4,
+        matchBrackets: true,
+        autoMatchParens: true,
         extraKeys: {"Tab": "indentMore", "Shift-Tab": "indentLess"}
     });
-    
+
+    return this.containerDiv;
+};
+
+SpeedType.prototype.appendElement = function(elem) {
+    elem.appendChild(this.containerDiv);
 };
