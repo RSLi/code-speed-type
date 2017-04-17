95791ad048d1b9316201708efb5749c0ad1f787f
RSLi <robertsli@163.com>
2017-04-17 14:35:12 -0400

    Add Webpack support for CodeMirror

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-require("jquery-ui");
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+import 'codemirror/lib/codemirror.js';
+import 'codemirror/mode/clike/clike.js';
 
+require("codemirror");
 $(document).ready(function() {
     alert("working");
 });
